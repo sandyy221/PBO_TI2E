3c83b42b52003400d879de9fae825821d1a5ebb5
san_wyd <kurniawansandy808@gmail.com>
2025-09-14 06:50:32 +0700

    tugasNo6_jobsheet3

--- tugasJobsheet3/testKoperasi.java
+++ tugasJobsheet3/testKoperasi.java
@@ -1,26 +1,22 @@
 package tugasJobsheet3;
+import java.util.Scanner;
 
 public class testKoperasi {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Anggota donny = new Anggota("111333444", "Donny", 5000000);
 
         System.out.println("Nama Anggota: " + donny.getNama());
         System.out.println("Limit Pinjaman: " + donny.getLimitPinjaman());
 
-        System.out.println("\nMeminjam uang 10.000.000...");
-        donny.pinjam(10000000);
+        System.out.print("\nMasukkan jumlah pinjaman: ");
+        int pinjam = sc.nextInt();
+        donny.pinjam(pinjam);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
 
-        System.out.println("\nMeminjam uang 4.000.000...");
-        donny.pinjam(4000000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-
-        System.out.println("\nMembayar angsuran 1.000");
-        donny.angsur(1000);
-        System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
-
-        System.out.println("\nMembayar angsuran 3.000.000");
-        donny.angsur(3000000);
+        System.out.print("\nMasukkan jumlah angsuran: ");
+        int angsur = sc.nextInt();
+        donny.angsur(angsur);
         System.out.println("Jumlah pinjaman saat ini: " + donny.getJumlahPinjaman());
     }
 }
